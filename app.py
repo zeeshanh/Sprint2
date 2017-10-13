fd71909a21469796f94634563352f961ea81d504
nmadd <ncmaddrey@gmail.com>
2017-10-13 14:13:15 -0400

    preparing for heroku deploy

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def timerHelper():
         socketio.emit("timerUpdate", timer)
         socketio.sleep(1)
         if timer==1:
-            winner = calculateWinner()
-            socketio.emit("winner", winner)
             timer = 100
             socketio.emit("timerUpdate", timer)
             stories = {}
             socketio.emit('updateStories', list(reversed(stories)))
+			winner = calculateWinner()
+			socketio.emit("winner", winner)
 
 
 @app.route("/<id>")
