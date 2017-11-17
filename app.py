9df62de2a2ad0989094b9fdf5d7a923e633e9131
Zeeshan Hanif <zeeshi13@gmail.com>
2017-11-16 22:51:37 -0500

    Timer fixed

--- app.py
+++ app.py
@@ -40,12 +40,14 @@ socketio = SocketIO(app)
 def setTimer():
     data = request.get_json()
     try:
-        time = int(data.get("time"))
+        time = int(data.get('time'))
     except:
         return Response(500)
 
     global timer 
+
     timer = time
+    print timer
     socketio.emit("timerUpdate", timer)
 
     return Response(200)
@@ -75,22 +77,21 @@ def getStories():
      #   return redirect(url_for("index"))
     #session.clear()
     #print session.get('user')
+    global numUsers
+    global users
     if session.get('user') is None:
         userID = uuid.uuid1()
         session['user'] = userID
 
-        global users
+        
         users.append(userID)
-
-        global numUsers
         numUsers+=1
 
         socketio.emit("newUser", numUsers)
     elif session.get('user') not in users:
-        global users
-        users.append(userID)
+        
+        users.append(session.get('user'))
 
-        global numUsers
         numUsers+=1
 
         socketio.emit("newUser", numUsers)
@@ -190,16 +191,18 @@ def calculateWinner():
 		return "No one "
 	winStory= reduce(lambda x, y : x if x.getUpvoteNum() > y.getUpvoteNum() else y, list(storyList.values()))
 	#winUser = users[winStory.ownerID]
-	return winUser.ownerID
+	return winStory.ownerID
 
 def readStories():
     f = open("stories.csv")
     strs = f.readlines()
+    global poolMoney
     for i, stry in enumerate(strs):
         temp = stry.split(",")
         if temp[0]=="":
             break
-        storyList[i] = (Story.Story(temp[2], i, "dsads", "https://pbs.twimg.com/profile_images/834788534053699584/4MIgR0Rl.jpg"))
+        poolMoney+=5
+        storyList[temp[0]] = (Story.Story(temp[2], temp[0], "dsads", "https://pbs.twimg.com/profile_images/834788534053699584/4MIgR0Rl.jpg"))
         print temp
     return
 
